414b2ec4d4645b91b175c9fe5a4fbc63ab73c90e
avinash6 <avinash.sg7@gmail.com>
2024-10-27 22:23:11 +0800

    Start correlation service. TODO: Outline algorithm

---
+++ backend/pkg/log/service/correlation_service.go
@@ -0,0 +1,41 @@
+package service
+
+type bucket int64
+
+const (
+	within100ms bucket = 100
+	within5s    bucket = 5000
+	within30s   bucket = 30000
+	within1m    bucket = 60000
+)
+
+type countInfo struct {
+	count       int64
+	occurrences int64
+}
+
+type CorrelationService struct {
+	// coefficient for count of co-occurrences
+	alpha float64
+	// coefficient for bucket size
+	beta float64
+}
+
+func NewCorrelationService() *CorrelationService {
+	return &CorrelationService{
+		alpha: 0.5,
+		beta:  0.5,
+	}
+}
+
+func (cs *CorrelationService) getConfidenceScore(countInfo countInfo, bucket bucket) float64 {
+	return ((cs.alpha * float64(countInfo.count)) + (cs.beta / float64(bucket))) / float64(countInfo.occurrences)
+}
+
+func (cs *CorrelationService) getConfidenceScores(serviceActionScores map[string]map[bucket]countInfo) {
+	for serviceAction, buckets := range serviceActionScores {
+		for bucket, countInfo := range buckets {
+			serviceActionScores[serviceAction][bucket] = cs.getConfidenceScore(countInfo, bucket)
+		}
+	}
+}
