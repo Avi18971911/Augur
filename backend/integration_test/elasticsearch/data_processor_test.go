ecf8a22d5734b861b99da9d35e66cd920fb78f59
avinash6 <avinash.sg7@gmail.com>
2024-11-27 22:02:33 +0800

    Add second test

--- backend/integration_test/elasticsearch/data_processor_test.go
+++ backend/integration_test/elasticsearch/data_processor_test.go
@@ -107,4 +107,88 @@ func TestDataProcessor(t *testing.T) {
 		assert.Equal(t, int64(3), clusterAEntries[0].Occurrences)
 		assert.Equal(t, int64(2), clusterBEntries[0].Occurrences)
 	})
+
+	t.Run("should increment asymmetrically with multiple overlaps on the same period", func(t *testing.T) {
+		clusterA := "clusterA"
+		clusterB := "clusterB"
+
+		firstTimeStamp := time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC)
+		overlapWithFirstTimeStampOne := time.Date(2021, 1, 1, 0, 0, 0, 500, time.UTC)
+		overlapWithFirstTimeStampTwo := time.Date(2021, 1, 1, 0, 0, 0, 750, time.UTC)
+		overlapWithFirstTimeStampThree := time.Date(2021, 1, 1, 0, 0, 0, 900, time.UTC)
+
+		clusterBMiss := time.Date(2023, 1, 1, 0, 0, 1, 0, time.UTC)
+
+		createdAt := time.Date(1992, 1, 1, 0, 0, 0, 0, time.UTC)
+
+		coOccurringLogs := []logModel.LogEntry{
+			{
+				ClusterId: clusterA,
+				Timestamp: firstTimeStamp,
+				CreatedAt: createdAt,
+			},
+			{
+				ClusterId: clusterB,
+				Timestamp: overlapWithFirstTimeStampOne,
+				CreatedAt: createdAt,
+			},
+			{
+				ClusterId: clusterB,
+				Timestamp: overlapWithFirstTimeStampTwo,
+				CreatedAt: createdAt,
+			},
+			{
+				ClusterId: clusterB,
+				Timestamp: overlapWithFirstTimeStampThree,
+				CreatedAt: createdAt,
+			},
+			{
+				ClusterId: clusterB,
+				Timestamp: clusterBMiss,
+				CreatedAt: createdAt,
+			},
+		}
+
+		err := loadDataIntoElasticsearch(ac, coOccurringLogs)
+		if err != nil {
+			t.Errorf("failed to load data into Elasticsearch: %v", err)
+		}
+
+		buckets := []countService.Bucket{100}
+		dp.ProcessData(context.Background(), buckets, dpIndices)
+
+		stringQuery, err := json.Marshal(getAllQuery())
+		if err != nil {
+			t.Errorf("failed to marshal query: %v", err)
+		}
+		var querySize = 100
+
+		searchCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		docs, err := ac.Search(searchCtx, string(stringQuery), []string{bootstrapper.CountIndexName}, &querySize)
+		if err != nil {
+			t.Errorf("Failed to search for count: %v", err)
+		}
+		countEntries, err := convertCountDocsToCountEntries(docs)
+		if err != nil {
+			t.Errorf("Failed to convert count docs to count entries: %v", err)
+		}
+
+		clusterAEntries := make([]countModel.CountEntry, 0)
+		clusterBEntries := make([]countModel.CountEntry, 0)
+
+		for _, entry := range countEntries {
+			if entry.ClusterId == clusterA {
+				clusterAEntries = append(clusterAEntries, entry)
+			} else if entry.ClusterId == clusterB {
+				clusterBEntries = append(clusterBEntries, entry)
+			}
+		}
+
+		assert.Equal(t, int64(1), clusterAEntries[0].CoOccurrences)
+		assert.Equal(t, int64(3), clusterBEntries[0].CoOccurrences)
+		
+		assert.Equal(t, int64(1), clusterAEntries[0].Occurrences)
+		assert.Equal(t, int64(4), clusterBEntries[0].Occurrences)
+	})
 }
