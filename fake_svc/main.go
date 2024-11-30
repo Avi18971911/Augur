0736d29b99a0644c2a3adb89676381e201cadb15
Shehbaz <shehbazk2812@gmail.com>
2024-11-30 13:07:23 +0800

    cleanup

--- fake_svc/main.go
+++ fake_svc/main.go
@@ -12,19 +12,17 @@ import (
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.20.0"
-	"go.uber.org/zap" // Import zap for structured logging
+	"go.uber.org/zap"
 )
 
 func initTracer() func() {
 	ctx := context.Background()
 
-	// Create OTLP trace exporter
 	exporter, err := otlptracehttp.New(ctx, otlptracehttp.WithEndpoint("http://otel-collector:4318"), otlptracehttp.WithInsecure())
 	if err != nil {
 		log.Fatalf("Failed to create trace exporter: %v", err)
 	}
 
-	// Create Resource
 	res, err := resource.New(ctx,
 		resource.WithAttributes(
 			semconv.ServiceNameKey.String("fake-service"),
@@ -34,7 +32,6 @@ func initTracer() func() {
 		log.Fatalf("Failed to create resource: %v", err)
 	}
 
-	// Create Tracer Provider
 	tp := sdktrace.NewTracerProvider(
 		sdktrace.WithSampler(sdktrace.AlwaysSample()),
 		sdktrace.WithBatcher(exporter),
@@ -49,7 +46,6 @@ func initTracer() func() {
 }
 
 func initLogger() *zap.Logger {
-	// Create zap logger
 	logger, err := zap.NewProduction()
 	if err != nil {
 		log.Fatalf("Failed to create logger: %v", err)
@@ -58,33 +54,27 @@ func initLogger() *zap.Logger {
 }
 
 func main() {
-	// Initialize tracer and logger
+
 	shutdownTracer := initTracer()
 	defer shutdownTracer()
 
-	// Initialize logger
 	logger := initLogger()
 
-	// Start tracer and log messages
 	tracer := otel.Tracer("fake-service")
 
 	for {
 		func() {
-			// Start a new span for each iteration
 			_, span := tracer.Start(context.Background(), "fake-operation")
 			defer span.End()
 
-			// Generate a random string as an operation ID
 			opID := randomString()
 			span.SetAttributes(attribute.String("operation.id", opID))
 
-			// Log a message with the trace ID and operation ID using zap
 			logger.Info("Log message with trace ID",
 				zap.String("trace_id", span.SpanContext().TraceID().String()),
 				zap.String("operation.id", opID),
 			)
 
-			// Simulate work with a sleep
 			time.Sleep(2 * time.Second)
 		}()
 	}
