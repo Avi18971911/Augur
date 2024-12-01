3712d095f05c6626d2b59bee61842446c8671e42
avinash6 <avinash.sg7@gmail.com>
2024-12-01 22:49:08 +0800

    Add tests

--- backend/integration_test/elasticsearch/count_mean_and_std_test.go
+++ backend/integration_test/elasticsearch/count_mean_and_std_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/Avi18971911/Augur/pkg/elasticsearch/bootstrapper"
 	"github.com/Avi18971911/Augur/pkg/elasticsearch/client"
 	"github.com/Avi18971911/Augur/pkg/log/model"
+	spanModel "github.com/Avi18971911/Augur/pkg/trace/model"
 	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
@@ -84,4 +85,221 @@ func TestMeandAndSTD(t *testing.T) {
 		expectedMean := (logOneSecondAfter.Timestamp.Sub(newLog.Timestamp).Seconds() + logTwoSecondsAfter.Timestamp.Sub(newLog.Timestamp).Seconds()) / 2
 		assert.Equal(t, expectedMean, countEntry.MeanTDOA)
 	})
+
+	t.Run("should maintain a running variance for the time difference of arrival for spans", func(t *testing.T) {
+		err := deleteAllDocuments(es)
+		if err != nil {
+			t.Errorf("Failed to delete all documents: %v", err)
+		}
+		initialTime := time.Date(2021, 1, 1, 0, 0, 0, 204, time.UTC)
+		newSpan := spanModel.Span{
+			ClusterId: "initialClusterId",
+			StartTime: initialTime,
+			EndTime:   initialTime.Add(time.Second),
+		}
+		spanOneSecondAfter := spanModel.Span{
+			ClusterId: "otherClusterId",
+			StartTime: initialTime.Add(time.Second),
+			EndTime:   initialTime.Add(time.Second * 2),
+		}
+		spanTwoSecondsAfter := spanModel.Span{
+			ClusterId: "otherClusterId",
+			StartTime: initialTime.Add(time.Second * 2),
+			EndTime:   initialTime.Add(time.Second * 3),
+		}
+		err = loadDataIntoElasticsearch(
+			ac, []spanModel.Span{
+				newSpan,
+				spanOneSecondAfter,
+				spanTwoSecondsAfter,
+			},
+			bootstrapper.SpanIndexName,
+		)
+		if err != nil {
+			t.Error("Failed to load spans into elasticsearch")
+		}
+		buckets := []countModel.Bucket{4500}
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		res, err := cs.GetCountAndUpdateOccurrencesQueryConstituents(
+			ctx,
+			newSpan.ClusterId,
+			countModel.TimeInfo{SpanInfo: &countModel.SpanInfo{FromTime: newSpan.StartTime, ToTime: newSpan.EndTime}},
+			indices,
+			buckets,
+		)
+		if err != nil {
+			t.Errorf("Failed to count occurrences: %v", err)
+		}
+		index := bootstrapper.CountIndexName
+		err = ac.BulkIndex(ctx, res.MetaMapList, res.DocumentMapList, &index)
+		if err != nil {
+			t.Errorf("Failed to insert records: %v", err)
+		}
+
+		searchQueryBody := countQuery(newSpan.ClusterId)
+		docs, err := ac.Search(ctx, searchQueryBody, []string{bootstrapper.CountIndexName}, &querySize)
+		if err != nil {
+			t.Errorf("Failed to search for count: %v", err)
+		}
+		countEntries, err := convertCountDocsToCountEntries(docs)
+		if err != nil {
+			t.Errorf("Failed to convert count docs to count entries: %v", err)
+		}
+
+		countEntry := countEntries[0]
+		expectedMean := (spanOneSecondAfter.StartTime.Sub(newSpan.StartTime).Seconds() + spanTwoSecondsAfter.StartTime.Sub(newSpan.StartTime).Seconds()) / 2
+		assert.Equal(t, expectedMean, countEntry.MeanTDOA)
+	})
+
+	t.Run(
+		"should maintain a running variance for the time difference of arrival for a mix of spans and logs",
+		func(t *testing.T) {
+			err := deleteAllDocuments(es)
+			if err != nil {
+				t.Errorf("Failed to delete all documents: %v", err)
+			}
+			initialTime := time.Date(2021, 1, 1, 0, 0, 0, 204, time.UTC)
+			newSpan := spanModel.Span{
+				ClusterId: "initialClusterId",
+				StartTime: initialTime,
+				EndTime:   initialTime.Add(time.Second),
+			}
+			logOneSecondAfter := model.LogEntry{
+				ClusterId: "otherClusterId",
+				Timestamp: initialTime.Add(time.Second),
+			}
+			logTwoSecondsAfter := model.LogEntry{
+				ClusterId: "otherClusterId",
+				Timestamp: initialTime.Add(time.Second * 2),
+			}
+			err = loadDataIntoElasticsearch(
+				ac, []spanModel.Span{
+					newSpan,
+				},
+				bootstrapper.SpanIndexName,
+			)
+			if err != nil {
+				t.Error("Failed to load spans into elasticsearch")
+			}
+			err = loadDataIntoElasticsearch(
+				ac, []model.LogEntry{
+					logOneSecondAfter,
+					logTwoSecondsAfter,
+				},
+				bootstrapper.LogIndexName,
+			)
+			if err != nil {
+				t.Error("Failed to load logs into elasticsearch")
+			}
+			buckets := []countModel.Bucket{4500}
+			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+			defer cancel()
+			res, err := cs.GetCountAndUpdateOccurrencesQueryConstituents(
+				ctx,
+				newSpan.ClusterId,
+				countModel.TimeInfo{SpanInfo: &countModel.SpanInfo{FromTime: newSpan.StartTime, ToTime: newSpan.EndTime}},
+				indices,
+				buckets,
+			)
+			if err != nil {
+				t.Errorf("Failed to count occurrences: %v", err)
+			}
+			index := bootstrapper.CountIndexName
+			err = ac.BulkIndex(ctx, res.MetaMapList, res.DocumentMapList, &index)
+			if err != nil {
+				t.Errorf("Failed to insert records: %v", err)
+			}
+
+			searchQueryBody := countQuery(newSpan.ClusterId)
+			docs, err := ac.Search(ctx, searchQueryBody, []string{bootstrapper.CountIndexName}, &querySize)
+			if err != nil {
+				t.Errorf("Failed to search for count: %v", err)
+			}
+			countEntries, err := convertCountDocsToCountEntries(docs)
+			if err != nil {
+				t.Errorf("Failed to convert count docs to count entries: %v", err)
+			}
+
+			countEntry := countEntries[0]
+			expectedMean := (logOneSecondAfter.Timestamp.Sub(newSpan.StartTime).Seconds() + logTwoSecondsAfter.Timestamp.Sub(newSpan.StartTime).Seconds()) / 2
+			assert.Equal(t, expectedMean, countEntry.MeanTDOA)
+		},
+	)
+
+	t.Run(
+		"should maintain a running variance for the time difference of arrival for a mix of logs and spans",
+		func(t *testing.T) {
+			err := deleteAllDocuments(es)
+			if err != nil {
+				t.Errorf("Failed to delete all documents: %v", err)
+			}
+			initialTime := time.Date(2021, 1, 1, 0, 0, 0, 204, time.UTC)
+			newLog := model.LogEntry{
+				ClusterId: "initialClusterId",
+				Timestamp: initialTime,
+			}
+			spanOneSecondAfter := spanModel.Span{
+				ClusterId: "otherClusterId",
+				StartTime: initialTime.Add(time.Second),
+				EndTime:   initialTime.Add(time.Second * 2),
+			}
+			spanTwoSecondsAfter := spanModel.Span{
+				ClusterId: "otherClusterId",
+				StartTime: initialTime.Add(time.Second * 2),
+				EndTime:   initialTime.Add(time.Second * 3),
+			}
+			err = loadDataIntoElasticsearch(
+				ac, []spanModel.Span{
+					spanOneSecondAfter,
+					spanTwoSecondsAfter,
+				},
+				bootstrapper.SpanIndexName,
+			)
+			if err != nil {
+				t.Error("Failed to load spans into elasticsearch")
+			}
+			err = loadDataIntoElasticsearch(
+				ac, []model.LogEntry{
+					newLog,
+				},
+				bootstrapper.LogIndexName,
+			)
+			if err != nil {
+				t.Error("Failed to load logs into elasticsearch")
+			}
+			buckets := []countModel.Bucket{4500}
+			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+			defer cancel()
+			res, err := cs.GetCountAndUpdateOccurrencesQueryConstituents(
+				ctx,
+				newLog.ClusterId,
+				countModel.TimeInfo{LogInfo: &countModel.LogInfo{Timestamp: newLog.Timestamp}},
+				indices,
+				buckets,
+			)
+			if err != nil {
+				t.Errorf("Failed to count occurrences: %v", err)
+			}
+			index := bootstrapper.CountIndexName
+			err = ac.BulkIndex(ctx, res.MetaMapList, res.DocumentMapList, &index)
+			if err != nil {
+				t.Errorf("Failed to insert records: %v", err)
+			}
+
+			searchQueryBody := countQuery(newLog.ClusterId)
+			docs, err := ac.Search(ctx, searchQueryBody, []string{bootstrapper.CountIndexName}, &querySize)
+			if err != nil {
+				t.Errorf("Failed to search for count: %v", err)
+			}
+			countEntries, err := convertCountDocsToCountEntries(docs)
+			if err != nil {
+				t.Errorf("Failed to convert count docs to count entries: %v", err)
+			}
+
+			countEntry := countEntries[0]
+			expectedMean := (spanOneSecondAfter.StartTime.Sub(newLog.Timestamp).Seconds() + spanTwoSecondsAfter.StartTime.Sub(newLog.Timestamp).Seconds()) / 2
+			assert.Equal(t, expectedMean, countEntry.MeanTDOA)
+		},
+	)
 }
