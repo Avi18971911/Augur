cd4354b02bde70ef4de0adeaffe04125ed2b3ad2
avinash6 <avinash.sg7@gmail.com>
2024-12-07 19:23:56 +0800

    Add second cluster test to ensure cluster events that are disimilar aren't clustered

--- backend/integration_test/elasticsearch/cluster_test.go
+++ backend/integration_test/elasticsearch/cluster_test.go
@@ -62,7 +62,7 @@ func TestSpanClusterUpdates(t *testing.T) {
 		}
 		output, err := cls.ClusterData(ctx, input)
 		if err != nil {
-			t.Errorf("Failed to parse span with message: %v", err)
+			t.Errorf("Failed to cluster span with error: %v", err)
 		}
 		assert.NotZero(t, len(output))
 		hasTest := output[0].ObjectId == "test"
@@ -79,6 +79,53 @@ func TestSpanClusterUpdates(t *testing.T) {
 	})
 
 	t.Run("should not assign spans with different cluster events to the same cluster id", func(t *testing.T) {
+		err := deleteAllDocuments(es)
+		if err != nil {
+			t.Errorf("Failed to delete all documents: %v", err)
+		}
+		const notAssigned = "NOT_ASSIGNED"
+		onlyTimeStamp := time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC)
+		createdAt := time.Date(1992, 1, 1, 0, 0, 0, 0, time.UTC)
+		const clusterEvent = "service=loadgenerator,operation=GET,kind=SPAN_KIND_CLIENT"
+		spanBatch := []spanModel.Span{
+			{
+				ClusterId:    notAssigned,
+				StartTime:    onlyTimeStamp,
+				EndTime:      onlyTimeStamp,
+				CreatedAt:    createdAt.Add(-time.Second * 500),
+				ClusterEvent: clusterEvent,
+				Events:       make([]spanModel.SpanEvent, 0),
+				Attributes:   make(map[string]string),
+			},
+			{
+				ClusterId:    notAssigned,
+				StartTime:    onlyTimeStamp,
+				EndTime:      onlyTimeStamp,
+				CreatedAt:    createdAt.Add(-time.Second * 400),
+				ClusterEvent: clusterEvent,
+				Events:       make([]spanModel.SpanEvent, 0),
+				Attributes:   make(map[string]string),
+			},
+		}
+		err = loadDataIntoElasticsearch(ac, spanBatch, bootstrapper.SpanIndexName)
+		if err != nil {
+			t.Errorf("Failed to load test data: %v", err)
+		}
 
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		input := clusterModel.ClusterInput{
+			DataType:    clusterModel.SpanClusterInputType,
+			TextualData: "not the same cluster event indeed",
+			ClusterId:   notAssigned,
+			Id:          "test",
+		}
+		output, err := cls.ClusterData(ctx, input)
+		if err != nil {
+			t.Errorf("Failed to cluster span with error: %v", err)
+		}
+		assert.NotZero(t, len(output))
+		assert.Equal(t, 1, len(output))
+		assert.Equal(t, "test", output[0].ObjectId)
 	})
 }
