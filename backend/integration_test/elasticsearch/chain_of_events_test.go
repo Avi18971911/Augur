f0e6181f6b73e744041e1e66e7a2d8dbf08f2318
avinash6 <avinash.sg7@gmail.com>
2024-12-19 00:44:06 +0800

    Fix PDF for negative values

--- backend/integration_test/elasticsearch/chain_of_events_test.go
+++ backend/integration_test/elasticsearch/chain_of_events_test.go
@@ -441,4 +441,37 @@ func TestChainOfEvents(t *testing.T) {
 		assert.Equal(t, logs[7], *graph[clusterIdB].LogOrSpanData.LogDetails)
 		assert.Equal(t, logs[8], *graph[clusterIdC].LogOrSpanData.LogDetails)
 	})
+
+	t.Run("should be able to handle real data", func(t *testing.T) {
+		err := deleteAllDocuments(es)
+		assert.NoError(t, err)
+		err = loadTestDataFromFile(es, bootstrapper.LogIndexName, "data/log_index.json")
+		assert.NoError(t, err)
+		err = loadTestDataFromFile(es, bootstrapper.ClusterIndexName, "data/cluster_index.json")
+		assert.NoError(t, err)
+		err = loadTestDataFromFile(es, bootstrapper.CountIndexName, "data/count_index.json")
+		assert.NoError(t, err)
+
+		createdAt, err := time.Parse(time.RFC3339Nano, "2024-12-18T14:33:40.872458799Z")
+		assert.NoError(t, err)
+		timestamp, err := time.Parse(time.RFC3339Nano, "2024-12-18T14:33:33.234410296Z")
+		assert.NoError(t, err)
+
+		spanOrLogDatum := analyticsModel.LogOrSpanData{
+			Id:        "61d5afe39d21cbea98aa5a395ac158a2564e2d0d1d9b0e300aeced2b67a0cc8f",
+			ClusterId: "66553fb7-2b8f-4002-9a15-c96d5ec00781",
+			LogDetails: &logModel.LogEntry{
+				Id:        "61d5afe39d21cbea98aa5a395ac158a2564e2d0d1d9b0e300aeced2b67a0cc8f",
+				ClusterId: "66553fb7-2b8f-4002-9a15-c96d5ec00781",
+				CreatedAt: createdAt,
+				Timestamp: timestamp,
+				Message:   "Invalid credentials for Login request with username: Yolo and password: Swag",
+				Severity:  "error",
+				Service:   "fake-server",
+			},
+		}
+		graph, err := as.GetChainOfEvents(context.Background(), spanOrLogDatum)
+		assert.NoError(t, err)
+		assert.NotNil(t, graph)
+	})
 }
