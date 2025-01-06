d65c816fc3e3735bc5362760b439cd53b467a7a7
avinash6 <avinash.sg7@gmail.com>
2025-01-06 21:46:44 +0800

    Add README

---
+++ fake_svc/elasticsearch_queries/log_index_unique_by_cluster_id.sh
@@ -0,0 +1,11 @@
+curl -X GET "http://localhost:9200/log_index/_search" -H "Content-Type: application/json" -d'
+{
+    "size": 0,
+  "aggs": {
+    "unique_cluster_ids": {
+      "cardinality": {
+        "field": "cluster_id"
+      }
+    }
+  }
+}'
